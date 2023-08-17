38b8bfd254afdb64050dffc39f45d9c29d11572f
Jalmar01 <villarrealjalmar@hotmail.com>
2023-08-16 22:29:20 -0500

    add models product

---
+++ src/tests/product.test.js
@@ -0,0 +1,105 @@
+const request = require('supertest')
+const app = require('../app')
+const Category = require('../models/Category')
+require('../models')
+
+const URL_PRODUCT = '/api/v1/products'
+const URL_USER = '/api/v1/users/login'
+let TOKEN
+let product
+let category
+let productId
+beforeAll(async () => {
+    const user = {
+        email:"sergio@gmail.com",
+        password:"sergio1234"
+    }
+    const res = await request(app)
+        .post(URL_USER)
+        .send(user)
+
+    TOKEN = res.body.token
+
+    const categoryBody = {
+        name:"Moto"
+    }
+    category = await Category.create(categoryBody)
+
+
+    product = {
+        title:"Suzuki",
+        description:"Gn-125",
+        price:1799.99,
+        categoryId:category.id
+    }
+})
+
+
+test("POST -> 'URL_PRODUCT' should return status code 201,res.body to be defined and  res.body.title === product.title", async() => {
+   
+    const res = await request(app)
+        .post(URL_PRODUCT)
+        .send(product)
+        .set('Authorization', `Bearer ${TOKEN}`)
+
+    productId = res.body.id
+    expect(res.status).toBe(201)
+    expect(res.body).toBeDefined()
+    expect(res.body.title).toBe(product.title)
+
+   
+})
+
+test("GET ALL-> 'URL_PRODUCT' should return status code 200,res.body.length === 1", async() => {
+   
+    const res = await request(app)
+        .get(URL_PRODUCT)
+        
+
+
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
+
+})
+
+test("DET ONE -> 'URL_PRODUCT/:id' should return status code 200,res.body to be defined and  res.body.title === product.title", async() => {
+   
+    const res = await request(app)
+        .get(`${URL_PRODUCT}/${productId}`)
+       
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.title).toBe(product.title)
+   
+})
+
+test("UPDATE -> 'URL_PRODUCT/:id' should return status code 200,res.body to be defined and  res.body.title === productUpdate.title", async() => {
+   const productUpdate = {
+     title:"Yamaha",
+     description:"DT-175",
+   }
+
+    const res = await request(app)
+        .put(`${URL_PRODUCT}/${productId}`)
+        .send(productUpdate)
+        .set('Authorization', `Bearer ${TOKEN}`)
+       
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.title).toBe(productUpdate.title)
+   
+})
+
+test("DELETE -> 'URL_PRODUCT/:id' should return status code 204", async() => {
+    
+     const res = await request(app)
+         .delete(`${URL_PRODUCT}/${productId}`)
+         .set('Authorization', `Bearer ${TOKEN}`)
+        
+     expect(res.status).toBe(204)
+ 
+     await category.destroy()
+    
+ })
+
