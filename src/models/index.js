b553a0f924d512429791199d34e2e1163cc07e0e
Jalmar01 <villarrealjalmar@hotmail.com>
2023-08-17 22:09:18 -0500

    add models cart and purchase

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,27 @@
 const User = require('./User')
 const Category = require('./Category')
 const Product = require('./Product')
+const Cart = require('./Cart')
+const Purchase = require('./Purchase')
 
 
 // Product -> categoryId
 Product.belongsTo(Category)
 Category.hasMany(Product)
+
+// cart -> userId
+Cart.belongsTo(User)
+User.hasMany(Cart)
+
+//cart -> productId
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
+
+
+//Pruchase -> userId
+Purchase.belongsTo(User)
+User.hasMany(Purchase)
+
+//Pruchase -> productIdId
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
